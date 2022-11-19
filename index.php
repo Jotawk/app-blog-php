ac379118c4b18656201a993cd40e214296195510
Jotawk <Jonathan.tawk@live.fr>
2022-11-19 15:31:13 +0100

    Mise en place du projet

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Blog</title>
+  <link rel="stylesheet" href="./public/css/style.css">
+  <script refer src="./public/js/index.js"></script>
+</head>
+
+<body>
+  <div class="container">
+    <?php require_once 'includes/header.php' ?>
+    <div class="content">
+    </div>
+    <?php require_once 'includes/footer.php' ?>
+  </div>
+</body>
+
+</html>
